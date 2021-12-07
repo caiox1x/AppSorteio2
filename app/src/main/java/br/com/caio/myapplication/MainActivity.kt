6648bc1b119c1cf99eedd2c3e07b406727ae1d7d
caiox1x <caiodocarmo00@gmail.com>
2021-12-07 19:39:44 -0300

    Commit Final

--- app/src/main/java/br/com/caio/myapplication/MainActivity.kt
+++ app/src/main/java/br/com/caio/myapplication/MainActivity.kt
@@ -2,32 +2,44 @@ package br.com.caio.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import br.com.caio.myapplication.R.id.ButtonMain
-import br.com.caio.myapplication.R.id.TextMain
+import br.com.caio.myapplication.R.id.buttonMain
+import br.com.caio.myapplication.R.id.textMain
+import java.lang.Math.random
 import kotlin.random.Random
 
-class MainActivity : AppCompatActivity()  {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(this.supportActionBar != null
-        ) this.supportActionBar!!.hide()
+        if (this.supportActionBar != null) {
+            this.supportActionBar!!.hide()
+        }
+        val button: Button = findViewById(R.id.buttonMain)
+        val texto: TextView = findViewById(R.id.textMain)
 
-        val button: Button = findViewById(ButtonMain)
-            button.setOnClickListener{
-                button.text = random().toString()
-        }
-        val txt: TextView = findViewById(TextMain)
-            txt.setOnClickListener{
-                txt.text = random().toString()
-        }
+
+        button.setOnClickListener(this)
+        texto.setOnClickListener(this)
 
     }
-    private fun random(): Int {
-        return Random.nextInt (50) + 1
+
+    override fun onClick(v: View?) {
+        if (v?.id == buttonMain || v?.id == textMain) {
+            val texto: TextView = findViewById(R.id.textMain)
+            texto.text = random().toString()
+
+        }
     }
+
+    fun random(): Int {
+        return Random.nextInt (999) + 1
+    }
+
+
 }
+
